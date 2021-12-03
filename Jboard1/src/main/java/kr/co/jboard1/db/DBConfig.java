e98935c50cfdc0fef938827afd402fd719c69388
kkh9372 <kkh9372@naver.com>
2021-12-03 09:55:06 +0900

    배포주소 업데이트

--- Jboard1/src/main/java/kr/co/jboard1/db/DBConfig.java
+++ Jboard1/src/main/java/kr/co/jboard1/db/DBConfig.java
@@ -15,9 +15,7 @@ public class DBConfig {
 		return instance;
 	}
 	// DB����
-	private final String HOST = "jdbc:mysql://54.180.160.240:3306/kkh9372";
-	//private final String HOST = "jdbc:mysql://3.36.92.141:8080/kkh9372";
-	//private final String HOST = "jdbc:mysql://52.79.248.246:8080/kkh9372";
+	private final String HOST = "jdbc:mysql://13.125.183.58:3306/kkh9372";
 	private final String USER = "kkh9372";
 	private final String PASS = "1234";
 	
